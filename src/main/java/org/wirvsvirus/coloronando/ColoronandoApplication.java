47e589dfdab8d0e8954e9017d4e361f01bbdd18e
Christian Ortiz <christian.ortiz@adesso.de>
2020-03-21 00:08:24 +0100

    initial commit

---
+++ src/main/java/org/wirvsvirus/coloronando/ColoronandoApplication.java
@@ -0,0 +1,13 @@
+package org.wirvsvirus.coloronando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColoronandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColoronandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wirvsvirus/coloronando/ColoronandoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wirvsvirus.coloronando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColoronandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
