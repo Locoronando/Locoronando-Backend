2df47ff5db2c225155b577acb1592d0af05939e5
Manuel Kollus <git@kollus.de>
2020-03-21 08:41:05 +0100

    Fix project name

--- src/main/java/org/wirvsvirus/locoronando/LocoronandoApplication.java
+++ src/main/java/org/wirvsvirus/locoronando/LocoronandoApplication.java
@@ -1,13 +1,13 @@
-package org.wirvsvirus.coloronando;
+package org.wirvsvirus.locoronando;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ColoronandoApplication {
+public class LocoronandoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ColoronandoApplication.class, args);
+		SpringApplication.run(LocoronandoApplication.class, args);
 	}
 
 }
