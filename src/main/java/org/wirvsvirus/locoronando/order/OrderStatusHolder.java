337eba9648fcc2fd866c8d44744e21c13abde372
Paul2708 <paul.hoger@gmx.net>
2020-03-22 10:21:00 +0100

    Add status wrapper

---
+++ src/main/java/org/wirvsvirus/locoronando/order/OrderStatusHolder.java
@@ -0,0 +1,15 @@
+package org.wirvsvirus.locoronando.order;
+
+/**
+ * Class description.
+ *
+ * @author Paul2708
+ */
+public class OrderStatusHolder {
+
+  private OrderStatus status;
+
+  public OrderStatus getStatus() {
+    return status;
+  }
+}
